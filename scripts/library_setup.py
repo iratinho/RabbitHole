f11413268fb6b5f66b788848dff0402f6ef47385
iratinho <iratinho.dev@gmail.com>
2022-09-19 22:46:15 +0100

    Adjust glfw setup script to not contaminate submodule with artifacts

--- scripts/library_setup.py
+++ scripts/library_setup.py
@@ -1,14 +1,16 @@
 import os
 import subprocess
 
+artifacts_directory = os.getcwd() + "/.build/generated_install"
+
 def glfw3_setup():
     os.chdir(os.getcwd() + "/external/glfw")
 
     # Set the install directory for current cmake proj
-    subprocess.check_call('cmake ' + '-DCMAKE_INSTALL_PREFIX:PATH=' + os.getcwd() + ' ' + '-B .build -S .')
+    subprocess.check_call('cmake ' + '-DCMAKE_INSTALL_PREFIX:PATH=' + artifacts_directory + "/glfw" + ' ' + '-B build -S .')
 
     # Install cmake so it generate config to be used with find_packge
-    subprocess.check_call('cmake --build .build --target install')
+    subprocess.check_call('cmake --build build --target install')
 
 def main():
     glfw3_setup()
