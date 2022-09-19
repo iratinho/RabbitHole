fc477a09f3608c846cc0c204020f86de911cd1af
iratinho <iratinho.dev@gmail.com>
2022-09-19 22:20:30 +0100

    Add library setup script

---
+++ scripts/library_setup.py
@@ -0,0 +1,17 @@
+import os
+import subprocess
+
+def glfw3_setup():
+    os.chdir(os.getcwd() + "/external/glfw-3.3.8")
+
+    # Set the install directory for current cmake proj
+    subprocess.check_call('cmake ' + '-DCMAKE_INSTALL_PREFIX:PATH=' + os.getcwd() + ' ' + '-B .build -S .')
+
+    # Install cmake so it generate config to be used with find_packge
+    subprocess.check_call('cmake --build .build --target install')
+
+def main():
+    glfw3_setup()
+
+if __name__ == '__main__':
+    main()
