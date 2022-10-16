3272122be2650d77fd5298f4e4f8237fb7731e38
iratinho <iratinho.dev@gmail.com>
2022-10-16 04:01:54 +0100

    Fix image view format and creation

--- src/app/src/renderer.cpp
+++ src/app/src/renderer.cpp
@@ -426,13 +426,12 @@ namespace app::renderer {
         // Create imageViews for swapchain images
         uint32_t image_count;
         vkGetSwapchainImagesKHR(logical_device_, swapchain_, &image_count, nullptr);
-        swapchain_image_views_.resize(image_count);
+        
+        std::vector<VkImage> images(image_count);
+        vkGetSwapchainImagesKHR(logical_device_, swapchain_, &image_count, images.data());
 
         for (uint32_t i = 0; i < image_count; ++i)
         {
-            VkImage image;
-            vkGetSwapchainImagesKHR(logical_device_, swapchain_, &image_count, &image);
-
             VkImageSubresourceRange resources_ranges;
             resources_ranges.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
             resources_ranges.layerCount = 1;
@@ -447,15 +446,15 @@ namespace app::renderer {
             image_view_create_info.components.b = VK_COMPONENT_SWIZZLE_B;
             image_view_create_info.components.a = VK_COMPONENT_SWIZZLE_A; // Does swapchain images need to set alpha? can we make it VK_COMPONENT_SWIZZLE_ZERO ?
             image_view_create_info.flags = 0;
-            image_view_create_info.format = VK_FORMAT_R8G8B8A8_SRGB;
-            image_view_create_info.image = image;
+            image_view_create_info.format = VK_FORMAT_B8G8R8A8_SRGB;
+            image_view_create_info.image = images[i];
             image_view_create_info.pNext = nullptr;
             image_view_create_info.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
             image_view_create_info.subresourceRange = resources_ranges;
             image_view_create_info.viewType = VK_IMAGE_VIEW_TYPE_2D;
             
-            vkCreateImageView(logical_device_, &image_view_create_info, nullptr, &image_view);
-            swapchain_image_views_[i] = image_view;
+            VkResult create_image_view_result = vkCreateImageView(logical_device_, &image_view_create_info, nullptr, &image_view);
+            swapchain_image_views_.push_back(image_view);
         }
         
         return result == VK_SUCCESS;
